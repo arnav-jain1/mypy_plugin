e42e8456da10e0ab4b3b6cc150d7dca35aff577d
Arnav <arnavjain20042@gmail.com>
2025-03-19 20:46:31 -0500

    method 3, storing all in a dict

--- benchmarks/test.py
+++ benchmarks/test.py
@@ -1,13 +1,7 @@
 import numpy as np 
-import pytest
-from typing import reveal_type
 
 # b = np.random.randint(10, size=(3, 4))
-a = np.array([[1,2,3], [4,5,6]])
-b = np.array([[1,2,3]])
-reveal_type(a)
-reveal_type(b)
-print(b.shape)
-
-c = a * b
-print(c)
+var1 = np.array([[[[1,2,3], [4,5,6]]]])
+print(var1.shape)
+var2 = np.array([[[]]])
+print(var2.shape)
--- tensor_plugin/plugin.py
+++ tensor_plugin/plugin.py
@@ -1,58 +1,23 @@
-from mypy.plugin import Plugin, FunctionContext, MethodContext
-from mypy.types import Instance, Type , TupleType, TypeVarType, AnyType, TypeOfAny, get_proper_type, LiteralType
-from mypy.nodes import TypeInfo, ARG_POS, Var, SYMBOL_FUNCBASE_TYPES, SymbolTableNode, IntExpr, ListExpr
-from mypy.plugins.common import add_method_to_class
-from mypy import nodes
-from typing import Tuple, List
+from typing import Callable, Dict, List, Optional, Tuple, Union, cast
+from mypy.plugin import Plugin, FunctionContext, MethodContext, MethodSigContext, AttributeContext, ClassDefContext
+from mypy.types import Type, Instance, AnyType, TypeOfAny, CallableType
+from mypy.nodes import Expression, ListExpr, TupleExpr, IntExpr, CallExpr, NameExpr, MemberExpr, RefExpr, AssignmentStmt
+from mypy.checker import TypeChecker
 
-
-
-array_types = ["numpy.random.mtrand.randint", "numpy._core.multiarray.array"]
-class CustomPlugin(Plugin):
-    def get_function_hook(self, fullname: str):
-        # print(f"debug fullname {fullname}")
-        if fullname in array_types:
-            return self.transform_array
-        return None
-
-    def transform_array(self, ctx: FunctionContext) -> Type:
-        # print(f"DEBUG: transform_ndarray called: {ctx}")
-        
-        input_type = ctx.api.named_type("numpy.ndarray")
-
-        # print(type(ctx.args[0][0]))
-        # print(ctx.args[0][0])
-        # print(ctx.arg_types[0][0])
-        # print(ctx.arg_kinds[0][0])
-        # print(ctx.arg_kinds[0])
-        # print(ctx.arg_types[0][0])
-        # print(ctx.context)
-        # print(ctx.api)
-
-        if ctx.args and ctx.args[0]:
-            
-            shape, ranks = self.infer_shape(ctx.args[0][0])
-
-            print(f"DEBUG: Inferred shape: {shape} with rank {ranks}")
-            literal_dims = [LiteralType(dim, ctx.api.named_generic_type("builtins.int", [])) for dim in shape]
-
-            shape_tuple = TupleType(literal_dims, fallback=ctx.api.named_generic_type("builtins.tuple", []))
-            # print(literal_dims)
-            print(shape_tuple)
-
-            ndarray_type = ctx.api.named_generic_type("numpy.ndarray", [shape_tuple])
-
-            return ndarray_type
-        else:
-            print("DEBUG: No arguments provided")   
-            return Instance(tensor_type.node, [])
-
+class NumPyArrayPlugin(Plugin):
+    def __init__(self, options) -> None:
+        super().__init__(options)
+        self.vars = dict()
+    
+    
     def infer_shape(self, node: ListExpr) -> Tuple[List[int], int]:
+        print(node)
+        """Infer the shape of a nested list expression."""
         current_nodes = [node]
         shape = []
         rank = 0
 
-        while True:
+        while current_nodes:
             # Check if all nodes at current level are ListExpr
             if not all(isinstance(n, ListExpr) for n in current_nodes):
                 break
@@ -65,8 +30,23 @@ class CustomPlugin(Plugin):
             # Flatten to next level of nodes
             current_nodes = [child for n in current_nodes for child in n.items]
 
-
-
         return shape, rank
+    
+    def get_dynamic_class_hook(self, fullname):
+        if fullname.strip() == "numpy._core.multiarray.array":
+            return self.reg_var
+        return None
+    
+    def reg_var(self, ctx):
+        shape, rank = self.infer_shape(ctx.call.args[0])
+        self.vars[ctx.name] = shape
+        self.print_vars()
+        return None
+    
+    def print_vars(self):
+        print("Current vars: ")
+        for key, val in self.vars.items():
+            print(f"\tVariable name: {key} Shape: {val}")
+
 def plugin(version):
-    return CustomPlugin
+    return NumPyArrayPlugin
