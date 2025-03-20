0715fec88fff17f2b22cd103560c375b0fa6d75e
Arnav <arnavjain20042@gmail.com>
2025-03-20 16:05:33 -0500

    addition :)

--- benchmarks/test.py
+++ benchmarks/test.py
@@ -1,7 +1,19 @@
 import numpy as np 
+from typing import reveal_type
 
-# b = np.random.randint(10, size=(3, 4))
-var1 = np.array([[[[1,2,3], [4,5,6]]]])
-print(var1.shape)
-var2 = np.array([[[]]])
-print(var2.shape)
+# var1 = np.array([[[[1,2,3], [4,5,6]]]])
+# print(var1.shape)
+# var2 = np.array([[[[1,2,3], [4,5,6]]]])
+
+
+# v = var1 + var1 + var1
+# reveal_type(var1)
+# reveal_type(v)
+
+a = np.array([1, 2, 3])
+b = np.array([4, 6]) 
+result = a + b
+
+print(a.shape)
+print(b.shape)
+print(result.shape)
--- tensor_plugin/plugin.py
+++ tensor_plugin/plugin.py
@@ -1,9 +1,9 @@
-from mypy.plugin import Plugin, FunctionContext, MethodContext
+from mypy.plugin import Plugin, FunctionContext, MethodContext, CheckerPluginInterface
 from mypy.types import Instance, Type , TupleType, TypeVarType, AnyType, TypeOfAny, get_proper_type, LiteralType
 from mypy.nodes import TypeInfo, ARG_POS, Var, SYMBOL_FUNCBASE_TYPES, SymbolTableNode, IntExpr, ListExpr
 from mypy.plugins.common import add_method_to_class
 from mypy import nodes
-from typing import Tuple, List
+from typing import Tuple, List, Literal
 
 
 
@@ -12,13 +12,80 @@ class CustomPlugin(Plugin):
     def get_function_hook(self, fullname: str):
         # print(f"debug fullname {fullname}")
         if fullname == "numpy._core.multiarray.array":
-            return self.transform_array
+            return self.base_array
+        return None
+    def get_method_hook(self, fullname):
+        # print(f"debug fullname {fullname}")
+        if fullname == "numpy.ndarray.__add__":
+            return self.add_array
         return None
 
-    def transform_array(self, ctx: FunctionContext) -> Type:
-        print(f"DEBUG: transform_ndarray called: {ctx}")
+    def add_array(self, ctx):
+
+        print(f"DEBUG: add ndarray called: {ctx}")
+        # Save the args
+        lhs = ctx.type
+        rhs = ctx.arg_types[0][0]
+
+        proper_lhs = get_proper_type(lhs)
+        proper_rhs = get_proper_type(rhs)
+
         
-        input_type = ctx.api.named_type("numpy.ndarray")
+        # If one or the other is just a constant
+        if (rhs.type.fullname == 'builtins.int'):
+            return proper_lhs
+        elif (lhs.type.fullname == 'builtins.int'):
+            return proper_rhs
+        # If both are same dim
+        elif proper_lhs == proper_rhs:
+            return proper_lhs
+        
+        # Otherwise they are unequal, check for broadcasting
+        else:
+            # Get the shapes as a list and the sizes
+            lhs_shape = lhs.args[0].items
+            rhs_shape = rhs.args[0].items
+            lhs_size = len(lhs_shape)
+            rhs_size = len(rhs_shape)
+
+            # If the lhs is bigger then set this to true (for later)
+            lhs_vs_rhs = True if lhs_size > rhs_size else False
+            output = []
+
+            # Go through (backwards) each element
+            for i in range(1, min(lhs_size, rhs_size)+1):
+                # If either equals 1, then put the opposite element in the output list
+                if lhs_shape[-i].value == 1:
+                    output.insert(0, rhs_shape[-i])
+                elif rhs_shape[-i].value == 1:
+                    output.insert(0, lhs_shape[-i])
+                # if they are both the same, put the element as well
+                elif lhs_shape[-i] == rhs_shape[-i]:
+                    output.insert(0, lhs_shape[-i])
+                # Otherwise, show that this is a shape mismatch error NOT DONE YETT
+                else:
+                    ctx.api.fail("Shape mismatch", ctx.context)
+                    return AnyType(TypeOfAny.from_error)
+            # Attach the remaining of whatever is bigger to the front
+            if lhs_vs_rhs:
+                output = lhs_shape[:lhs_size-rhs_size] + output
+            else:
+                output = rhs_shape[:rhs_size-lhs_size] + output
+            
+            # return the final typing
+            shape_tuple = TupleType(output, fallback=ctx.api.named_generic_type("builtins.tuple", []))
+            final_type = ctx.api.named_generic_type("numpy.ndarray", [shape_tuple])
+            print(f"Final output: {final_type}")
+
+            return final_type
+
+
+
+
+
+
+    def base_array(self, ctx: FunctionContext) -> Type:
+        print(f"DEBUG: array() called: {ctx}")
 
         if ctx.args and ctx.args[0] and ctx.args[0][0]:
             
@@ -28,12 +95,10 @@ class CustomPlugin(Plugin):
             literal_dims = [LiteralType(dim, ctx.api.named_generic_type("builtins.int", [])) for dim in shape]
 
             shape_tuple = TupleType(literal_dims, fallback=ctx.api.named_generic_type("builtins.tuple", []))
-            # print(literal_dims)
-            # print(shape_tuple)
 
             final_type = ctx.api.named_generic_type("numpy.ndarray", [shape_tuple])
-            print(f"Type: {ndarray_type}")
-            # print(final_type.args[0])
+            print(f"Type: {final_type}")
+            
 
             return final_type
         else:
